95762b7d179a3871ee1c630def8cce1cd537cafd
Farhan_nr <farhannr75@gmail.com>
2023-11-24 20:54:49 +0700

    Auto stash before merge of "development" and "origin/alam"

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -3,8 +3,8 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="{{ route('dashboard') }}">
-            <img src="{{ asset('./img/logo-ct.png') }}" class="navbar-img h-100" alt="logo-ct">
+        <a class="navbar-brand m-0" href="{{ route('dashboard') }}" >
+            <img src="./img/logo-ct.png" class="navbar-img h-100" alt="logo-ct">
             <span class="ms-1 font-weight-bold">Bazma Web</span>
         </a>
     </div>
