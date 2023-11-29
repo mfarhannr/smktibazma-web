7da0c1470d0001802ebe877819d3ba7ee06d7bc9
Farhan_nr <farhannr75@gmail.com>
2023-11-29 08:55:36 +0700

    Perbaiki layout dan admin-management

---
+++ .history/app/Http/Controllers/UserController_20231129073809.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::get();
+        return view('admin-pages.user-managements.index', ['user' => $user]);
+    }
+
+    public function create()
+    {
+        return view('admin-pages.user-managements.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
+
+        User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password,
+            'role' => $request->role
+        ]);
+
+        return redirect()->to('/admin-management')->with('success', 'Admin berhasil ditambahkan');
+    }
+
+    public function destroy(string $id)
+    {
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect()->to('/admin-management')->with('success', 'Admin berhasil dihapus');
+    }
+}
