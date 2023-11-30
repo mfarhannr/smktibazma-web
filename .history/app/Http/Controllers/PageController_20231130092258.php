d8be57cf5f5d41e31f285802b321026695dbac3b
Farhan_nr <farhannr75@gmail.com>
2023-11-30 21:51:53 +0700

    merapihkan layout

---
+++ .history/app/Http/Controllers/PageController_20231130092258.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use App\Models\talent;
+// use App\Models\PPDB;
+// use App\Models\Katalogtalenta;
+
+// use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    /**
+     * Display all the static pages when authenticated
+     *
+     * @param string $page
+     * @return \Illuminate\View\View
+     */
+    // public function index(string $page)
+    // {
+    //     if (view()->exists("pages.{$page}")) {
+    //         return view("pages.{$page}");
+    //     }
+
+    //     return abort(404);
+    // }
+
+    public function beranda()
+    {
+        return view("pages.beranda");
+    }
+
+    public function profil()
+    {
+        return view("pages.profil");
+    }
+
+    public function kegiatan()
+    {
+        $katalogtalenta = katalogtalenta::get();
+        // return view("pages.katalogtalenta")->with('katalogtalenta', $katalogtalenta);
+        return view("pages.kegiatan");
+    }
+    public function galeri()
+    {
+        return view("pages.galeri");
+    }
+    public function talent()
+    {
+        // $allTalent = talent::orderBy('id','asc')->paginate(4);
+        // return view("pages.talent")->with('allTalent', $allTalent);
+
+        // $katalogtalenta = katalogtalenta::get();
+        // return view("pages.katalogtalenta")->with('katalogtalenta', $katalogtalenta);
+    }
+
+    public function team()
+    {
+        return view("pages.team");
+    }
+
+    public function donasi()
+    {
+        return view("pages.donasi");
+    }
+
+    public function ppdb()
+    {
+        // $ppdb = PPDB::get();
+        // return view("pages.ppdb")->with('ppdb', $ppdb);
+    }
+
+    public function mitra()
+    {
+        return view("pages.mitra");
+    }
+
+}
---
+++ .history/resources/views/layouts/navbars/auth/sidenav.blade_20231130205522.php
@@ -0,0 +1,248 @@
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/8cf86726f0.js" crossorigin="anonymous"></script>
+    
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
+    id="sidenav-main">
+    <div class="sidenav-header">
+        <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+            aria-hidden="true" id="iconSidenav"></i>
+        <a class="navbar-brand m-0" href="{{ route('dashboard') }}">
+            <img src="{{ asset('./img/logo-ct.png') }}" class="navbar-img h-100" alt="logo-ct">
+            <span class="ms-1 font-weight-bold">Bazma Web</span>
+        </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link {{ Route::currentRouteName() == 'dashboard' ? 'active' : '' }}"
+                    href="{{ route('dashboard') }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-shop text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ str_contains(request()->url(), 'admin-management') == true ? 'active' : '' }}"
+                    href=
+                    "{{ route('page', ['page' => 'admin-management']) }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-ungroup text-dark"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Admin Management</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ str_contains(request()->url(), 'kegiatan') == true ? 'active' : '' }}"
+                    href=
+                    "{{ route('page', ['page' => 'kegiatan']) }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-ungroup text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Kegiatan</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ str_contains(request()->url(), 'admin/prestasi') == true ? 'active' : '' }}"
+                    href=
+                    "{{ route('page', ['page' => 'page-prestasi']) }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-ungroup text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Prestasi</span>
+                </a>
+            </li>
+            {{-- <li class="nav-item mt-3 d-flex align-items-center">
+                <div class="ps-4">
+                    <i class="fab fa-laravel" style="color: #0057B8;"></i>
+                </div>
+                <h6 class="ms-2 text-uppercase text-xs font-weight-bolder opacity-6 mb-0">Laravel Examples</h6>
+            </li> --}}
+            {{-- <li class="nav-item">
+                <a class="nav-link {{ Route::currentRouteName() == 'profile' ? 'active' : '' }}" href="{{ route('profile') }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Profile</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#dashboardsExamples" class="nav-link collapsed"
+                    aria-controls="dashboardsExamples" role="button" aria-expanded="false">
+                    <div class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center">
+                        <i class="ni ni-shop text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li> --}}
+            <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#laravelExamples" class="nav-link "
+                    aria-controls="laravelExamples" role="button" aria-expanded="true">
+                    <div class="icon icon-shape icon-sm text-center d-flex align-items-end justify-content-center">
+                        <i class="fab fa-laravel" style="color: #f4645f; font-size: large; font-weight: 500 "
+                            aria-hidden="true"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Laravel Examples </span>
+                </a>
+                <div class="collapse show" id="laravelExamples" style="">
+                    <ul class="nav ms-4">
+                        <li class="nav-item ">
+                            <a class="nav-link "
+                                href="https://argon-dashboard-pro-laravel.creative-tim.com/user-profile">
+                                <span class="sidenav-mini-icon"> U </span>
+                                <span class="sidenav-normal"> User Profile </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link "
+                                href="https://argon-dashboard-pro-laravel.creative-tim.com/user-management">
+                                <span class="sidenav-mini-icon"> U </span>
+                                <span class="sidenav-normal"> User Management </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link "
+                                href="https://argon-dashboard-pro-laravel.creative-tim.com/role-management">
+                                <span class="sidenav-mini-icon"> R </span>
+                                <span class="sidenav-normal"> Role Management </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link "
+                                href="https://argon-dashboard-pro-laravel.creative-tim.com/category-management">
+                                <span class="sidenav-mini-icon"> C </span>
+                                <span class="sidenav-normal"> Category Management </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link "
+                                href="https://argon-dashboard-pro-laravel.creative-tim.com/tag-management">
+                                <span class="sidenav-mini-icon"> T </span>
+                                <span class="sidenav-normal"> Tag Management </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link "
+                                href="https://argon-dashboard-pro-laravel.creative-tim.com/item-management">
+                                <span class="sidenav-mini-icon"> I </span>
+                                <span class="sidenav-normal"> Items Management </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ str_contains(request()->url(), 'kategori-postingan') == true ? 'active' : '' }}"
+                    href="{{ route('page', ['page' => 'kategori-postingan']) }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-bullet-list-67 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Kategori Postingan</span>
+                </a>
+            </li>
+            {{-- <li class="nav-item mt-3">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Pages</h6>
+            </li> --}}
+
+            <li class="nav-item">
+                <a class="nav-link {{ str_contains(request()->url(), 'tables') == true ? 'active' : '' }}"
+                    href=
+                    "{{ route('page', ['page' => 'tables']) }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-calendar-grid-58 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Postingan</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ str_contains(request()->url(), 'katalogtalenta') == true ? 'active' : '' }}"
+                    href=
+                    "{{ route('page', ['page' => 'billing']) }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Katalog Talent</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Route::currentRouteName() == 'virtual-reality' ? 'active' : '' }}" href=#
+                    {{-- "{{ route('virtual-reality') }}" --}}>
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-app text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">PPDB</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Route::currentRouteName() == 'rtl' ? 'active' : '' }}" href=#
+                    {{-- "{{ route('rtl') }}" --}}>
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-world-2 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Galeri</span>
+                </a>
+            </li>
+            {{-- <li class="nav-item mt-3">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Route::currentRouteName() == 'profile-static' ? 'active' : '' }}"
+                    href="{{ route('profile-static') }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Profile</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="{{ route('sign-in-static') }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Sign In</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="{{ route('sign-up-static') }}">
+                    <div
+                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-collection text-info text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Sign Up</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="sidenav-footer mx-3 ">
+        <div class="card card-plain shadow-none" id="sidenavCard">
+            <img class="w-50 mx-auto" src="/img/illustrations/icon-documentation-warning.svg"
+                alt="sidebar_illustration">
+            <div class="card-body text-center p-3 w-100 pt-0">
+                <div class="docs-info">
+                    <h6 class="mb-0">Need help?</h6>
+                    <p class="text-xs font-weight-bold mb-0">Please check our docs</p>
+                </div>
+            </div>
+        </div>
+        <a href="/docs/bootstrap/overview/argon-dashboard/index.html" target="_blank"
+            class="btn btn-dark btn-sm w-100 mb-3">Documentation</a>
+        <a class="btn btn-primary btn-sm mb-0 w-100"
+            href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank"
+            type="button">Upgrade to PRO</a>
+    </div> --}}
+</aside>
